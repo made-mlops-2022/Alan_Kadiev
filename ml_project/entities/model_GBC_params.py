abed08c474703e10312e7b4c6b9c952bf1ff978a
howardano <58511197+howardano@users.noreply.github.com>
2022-11-21 00:09:32 +0300

    Delete ml_project/entities directory

--- ml_project/entities/model_GBC_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class ModelGBCParams:
-    n_estimators: int = field(default=50)
-    random_state: int = field(default=13)
-    max_depth: int = field(default=5)
--- ml_project/entities/split_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class SplittingParams:
-    val_size: float = field(default=0.2)
-    random_state: int = field(default=13)
-    shuffle: bool = field(default=True)
